38cef81aaf2622563b4e16fd505f151ae24c6def
Asadell <m.satrioasadell@gmail.com>
2023-06-15 20:19:10 +0700

    math beres

--- Section 06 Professional Project Maths Game (HTML, CSS & JavaScript)/javascript.js
+++ Section 06 Professional Project Maths Game (HTML, CSS & JavaScript)/javascript.js
@@ -2,16 +2,17 @@ let playing = false;
 let score;
 let timeremaining;
 let action;
+let correctanswer;
 
 
 // if we click on the start/reset
-document.getElementById("startreset").onclick = function(){
+document.getElementById("startreset").onclick = function () {
     // if we are playing
     if (playing) {
-        
+
         // reload page
         location.reload();
-        
+
     } else {// if we are not playing
         // menghilangkan panel game over
         hide("gameover");
@@ -31,9 +32,9 @@ document.getElementById("startreset").onclick = function(){
 
         // reduce time by 1sec in loops
         startCountdown();
-            // time left?
-                // yes -> continue
-                // no -> gameover
+        // time left?
+        // yes -> continue
+        // no -> gameover
 
         //change button to reset
         document.getElementById("startreset").innerHTML = "Reset Game";
@@ -45,51 +46,79 @@ document.getElementById("startreset").onclick = function(){
 }
 
 // if we click on answer box
-    // if we are playing
-        // correct?
-            // yes
-                // increase score
-                // show correct box for 1sec
-                // generate new QnA
-            // no
-                // show try again box for 1sec
+// if we are playing
+// correct?
+// yes
+// increase score
+// show correct box for 1sec
+// generate new QnA
+// no
+// show try again box for 1sec
 
 
 
 
 // if we click on the start/reset
-    // if we are playing
-        // reload page
-    // if we are not playing
-        // set score to 0
-        // shjow countdown box
-        // reduce time by 1sec in loops
-            // time left?
-                // yes -> continue
-                // no -> gameover
-        //change button to reset
-        // generate new QnA
+// if we are playing
+// reload page
+// if we are not playing
+// set score to 0
+// shjow countdown box
+// reduce time by 1sec in loops
+// time left?
+// yes -> continue
+// no -> gameover
+//change button to reset
+// generate new QnA
 
 // if we click on answer box
-    // if we are playing
-        // correct?
-            // yes
-                // increase score
-                // show correct box for 1sec
-                // generate new QnA
-            // no
-                // show try again box for 1sec
+// if we are playing
+// correct?
+// yes
+// increase score
+// show correct box for 1sec
+// generate new QnA
+// no
+// show try again box for 1sec
 
 // function
 
+for (let index = 1; index < 5; index++) {
+    document.getElementById("box" + index).onclick = function () {
 
-function startCountdown(){
+        if (playing) { // mode playing?
+
+            if (this.innerHTML == correctanswer) { // jawaban user benar
+                score++;
+                document.getElementById("scoreValue").innerHTML = score;
+
+                hide("wrong");
+                show("correct");
+                setTimeout(function () {
+                    hide("correct");
+                }, 1000);
+
+                generateQA();
+            } else { // jawaban user salah
+                hide("correct");
+                show("wrong");
+                setTimeout(function () {
+                    hide("wrong");
+                }, 1000);
+            }
+
+        }
+    }
+}
+
+
+function startCountdown() {
     action = setInterval(() => { // kurang tulisan function nyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         timeremaining--;
         document.getElementById("timeremainingvalue").innerHTML = timeremaining;
 
         // if timeremaining finished
-        if(timeremaining == 57){ // gameover
+        if (timeremaining == 0) { // gameover
             stopCountDown();
 
             hide("correct");
@@ -97,9 +126,9 @@ function startCountdown(){
             hide("timeremaining");
 
             document.getElementById("gameovervalue").innerHTML = score;
-            
+
             show("gameover");
-            
+
             document.getElementById("startreset").innerHTML = "Start Game";
 
             // change mode to finish
@@ -109,17 +138,37 @@ function startCountdown(){
     }, 1000);
 }
 
-function stopCountDown(){
+function stopCountDown() {
     clearInterval(action);
 }
 
-function show(id){
+function show(id) {
     document.getElementById(id).style.display = "block";
 }
-function hide(id){
+function hide(id) {
     document.getElementById(id).style.display = "none";
 }
 
-function generateQA(){
+function generateQA() {
+    let x = 1 + Math.round(Math.random() * 9);
+    let y = 1 + Math.round(Math.random() * 9);
+    correctanswer = x * y;
 
+    document.getElementById("question").innerHTML = x + "x" + y;
+
+    let correctposition = 1 + Math.round(Math.random() * 3);
+    document.getElementById("box" + correctposition).innerHTML = correctanswer;
+
+    let array = [correctanswer];
+
+    for (let index = 1; index < 5; index++) {
+        if (index != correctposition) {
+            let wronganswer;
+            do {
+                wronganswer = 1 + Math.round(Math.random() * 9) * 1 + Math.round(Math.random() * 9);
+            } while (array.indexOf(wronganswer) > -1);
+            document.getElementById("box" + index).innerHTML = wronganswer;
+            array.push(wronganswer);
+        }
+    }
 }
